dcbf86cf8b8d1dea074205168557db46927a79f6
Aritra Chakraborty <98593312+aritraChakraborty101@users.noreply.github.com>
2023-11-30 21:52:55 +0600

    Create selectionSort.py

---
+++ easy/selectionSort.py
@@ -0,0 +1,8 @@
+def selectionSort(array):
+    for i in range(len(array) - 1):
+        min = i
+        for j in range(i + 1, len(array)):
+            if array[min] > array[j]:
+                min = j
+        array[min], array[i] = array[i], array[min]
+    return array
